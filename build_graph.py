755680b7f06f9080bb091fdb558ba5e9bf4626ba
Himangshu Ranjan Borah <himangshu.nits@gmail.com>
2016-04-19 19:53:04 -0400

    Temp changes for item matrix calculation.

--- build_graph.py
+++ build_graph.py
@@ -13,5 +13,68 @@ import colour
 import build_similarity_matrices as bsm
 
 
+
+
+
+################################################################################################
+# CODE FOR VISUALIZATION OR THE CLUSTERS
+################################################################################################
+
+
+# Function to plot an igraph object.
+def sac_plot(sac_graph):
+	layout = sac_graph.layout("kk")
+	gp.plot(sac_graph, layout=layout)
+
+
+# Fucntion to plot an I graph object with different clusters having different colors according to a membership list.
+def sac_plot_cluster(sac_graph, membership_list, filename):
+	red = colour.Color("red")
+	blue = colour.Color("blue")
+	my_rainbow = list(red.range_to(blue, membership_list.max() + 1))
+	color_list = [i.get_hex_l() for i in my_rainbow]
+	layout = sac_graph.layout("kk")
+	gp.plot(sac_graph, filename, layout=layout, vertex_color=[color_list[x] for x in membership_list])
+
+# Same as above function, but from a cluster_dict. Saves the plot to filename.
+def plot_clusters_from_dict(clusters_dict, sac_graph_original, filename):
+	membership_list = [0] * len(sac_graph_original.vs)
+	membership_list = pd.Series(membership_list)
+	for key in clusters_dict.iterkeys():
+		indices = list(clusters_dict[key])
+		cluster_id = key
+		for index in indices:
+			membership_list[index] = cluster_id
+	sac_plot_cluster(sac_graph_original, membership_list, filename)		
+
+
+
+
+
+################################################################################################
+# CODE FOR BUILDING THE MAIN GRAPH
+################################################################################################
+
+
+def main():	
+	build_graph_from_matrices()
+
+
+
 def build_graph_from_matrices():
-	
+	[pearson_matrix_user, cosine_matrix_user] = bsm.calculate_matrices_user()
+	#print pearson_matrix_user
+	no_of_users = len(pearson_matrix_user)
+	user_graph = gp.Graph.Full
+
+
+
+
+
+
+
+
+# Call the main. Entry point.
+
+if __name__ == "__main__":
+	main()
--- build_similarity_matrices.py
+++ build_similarity_matrices.py
@@ -4,30 +4,30 @@ import subprocess
 import reading as rd
 import math
 
+MAX_SIZE_USER = 100
+MAX_SIZE_ITEM = 100
+
 #print rd.movies
 
-def calculate_matrices():
-    cosine = np.zeros(shape=(6041,6041))
+def calculate_matrices_user():
+    cosine = np.zeros(shape=(MAX_SIZE_USER,MAX_SIZE_USER))
 
-    pearson = np.zeros(shape=(6041,6041))
+    pearson = np.zeros(shape=(MAX_SIZE_USER,MAX_SIZE_USER))
 
 
     r_mean = rd.ratings.groupby(['UserID'])['Rating'].mean()
 
 
-    for ra in range(1,6041):
+    for ra in range(1,MAX_SIZE_USER):
         
         print ra
         #temp1 = rd.ratings[rd.ratings.UserID==ra].MovieID
         temp1 = rd.ratings[rd.ratings.UserID==ra].loc[:,['MovieID','Rating']]
-        for rb in range(ra,6041):
-            print rb
-            
+        for rb in range(ra,MAX_SIZE_USER):
+            print rb            
             #temp2 = rd.ratings[rd.ratings.UserID==rb].MovieID
-            temp2 = rd.ratings[rd.ratings.UserID==rb].loc[:,['MovieID','Rating']]
-            
-            temp3=pd.merge(temp1, temp2, how='inner', on=['MovieID'])      
-            
+            temp2 = rd.ratings[rd.ratings.UserID==rb].loc[:,['MovieID','Rating']]            
+            temp3=pd.merge(temp1, temp2, how='inner', on=['MovieID'])              
             
             num_pcc = 0.0
             denom_pcc =0.0
@@ -45,20 +45,85 @@ def calculate_matrices():
             rai = temp3.Rating_x
             rbi = temp3.Rating_y
             
-            num_pcc = sum ((rai - r_mean[ra]) * (rbi - r_mean[rb]))
-            
+            num_pcc = sum ((rai - r_mean[ra]) * (rbi - r_mean[rb]))            
                 
             d1_pcc = sum((rai - r_mean[ra]) ** 2 )
             d2_pcc = sum((rbi - r_mean[rb]) ** 2 )
             
             num_cos = sum(rai * rbi)
             d1_cos = sum(rai ** 2)
-            d2_cos = sum(rbi ** 2)                   
-            
-              
+            d2_cos = sum(rbi ** 2)
+
             denom_pcc = math.sqrt(d1_pcc) * math.sqrt(d2_pcc)     
-            denom_cos = math.sqrt(d1_cos) * math.sqrt(d2_cos)     
+            denom_cos = math.sqrt(d1_cos) * math.sqrt(d2_cos)       
         
+            if(denom_pcc==0.0):
+                denom_pcc =1.0
+        
+            if(denom_cos==0.0):
+                denom_cos =1.0
+
+
+            pearson[ra][rb] = num_pcc/denom_pcc
+            cosine[ra][rb] = num_cos/denom_cos
+            
+            pearson[rb][ra] = pearson[ra][rb]
+            cosine[rb][ra] = cosine[ra][rb]
+     
+    return pearson, cosine
+
+
+
+
+
+
+def calculate_matrices_item():
+    cosine = np.zeros(shape=(MAX_SIZE_ITEM,MAX_SIZE_ITEM))
+
+    pearson = np.zeros(shape=(MAX_SIZE_ITEM,MAX_SIZE_ITEM))
+
+
+    r_mean = rd.ratings.groupby(['MovieID'])['Rating'].mean()
+
+
+    for ra in range(1,MAX_SIZE_ITEM):
+        
+        print ra
+        #temp1 = rd.ratings[rd.ratings.UserID==ra].MovieID
+        temp1 = rd.ratings[rd.ratings.MovieID==ra].loc[:,['UserID','Rating']]
+        for rb in range(ra,MAX_SIZE_ITEM):
+            print rb            
+            #temp2 = rd.ratings[rd.ratings.UserID==rb].MovieID
+            temp2 = rd.ratings[rd.ratings.MovieID==rb].loc[:,['UserID','Rating']]            
+            temp3=pd.merge(temp1, temp2, how='inner', on=['UserID'])              
+            
+            num_pcc = 0.0
+            denom_pcc =0.0
+            
+            d1_pcc = 0.0
+            d1_cos = 0.0
+            
+            d2_pcc = 0.0
+            d2_cos = 0.0
+            
+            num_cos = 0.0
+            
+            denom_cos = 0.0            
+            
+            rai = temp3.Rating_x
+            rbi = temp3.Rating_y
+            
+            num_pcc = sum ((rai - r_mean[ra]) * (rbi - r_mean[rb]))            
+                
+            d1_pcc = sum((rai - r_mean[ra]) ** 2 )
+            d2_pcc = sum((rbi - r_mean[rb]) ** 2 )
+            
+            num_cos = sum(rai * rbi)
+            d1_cos = sum(rai ** 2)
+            d2_cos = sum(rbi ** 2)
+
+            denom_pcc = math.sqrt(d1_pcc) * math.sqrt(d2_pcc)     
+            denom_cos = math.sqrt(d1_cos) * math.sqrt(d2_cos)       
         
             if(denom_pcc==0.0):
                 denom_pcc =1.0
