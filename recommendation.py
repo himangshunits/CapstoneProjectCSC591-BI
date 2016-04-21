e93bc7a8950064aba0479ca60ee4cbae8f034024
Siddhant Doshi <siddhant@Siddhants-MacBook-Pro.local>
2016-04-20 21:27:29 -0400

    Recommendation

--- recommendation.py
+++ recommendation.py
@@ -2,55 +2,73 @@ import ups_clustering as uc
 import numpy as np
 import pandas as pd
 import reading as rd
+import math
 
-k = 10
-t = 1
-i = 10
+k = 5
+t = 62
+i = 257
+
+
+MAX_SIZE_USER = len(rd.users) + 1
+MAX_SIZE_ITEM = len(rd.movies) + 1
 
 r_mean = rd.ratings.groupby(['UserID'])['Rating'].mean()
-sim = uc.user_ups
+sim = np.load("user_ups.npy")
+user_item = pd.read_csv("user_item.csv")
 
 U_o = uc.o_cluster
 U_p = uc.p_cluster
 U_n = uc.n_cluster
 
-#print U_o, U_n , U_p
-print "Hello"
-
-
 U_sample = []
 
-
-if user_id in U_o:
+if t in U_o:
 	U_sample = U_o + U_n
-elif user_id in U_p:
+elif t in U_p:
 	U_sample = U_p + U_n
 else:
 	U_sample = U_n
-
 
 print len(U_o)
 print len(U_n)
 print len(U_p)
 print len(U_sample)
 
+sim_u = []
+for u in U_sample:
+	sim_u += [(sim[u][t], u)]
 
-'''
+sim_u = sorted(sim_u, reverse = True)
+U_nei = [x[1] for x in sim_u[:k+1]]
+
 #calculate U_nei
-U_nei = [1,2,3,4,5]
+#U_nei = [1,2,3,4,5]
 
-user_item = pd.read_csv("user_item.csv")
-prediction = r_mean[t]
+#prediction
+list_predictions = []
+# recommendation for user t
+for i in range(1, MAX_SIZE_ITEM):
+	rti = user_item.ix[t][i]
 
-prediction_second_num =0.0
-prediction_second_denom =0.0
 
-for u in U_nei:
-	rui = user_item.ix[u][i]
-	if !Math.isnan(rui):
-		prediction_second_num += ((rui - r_mean[u]) * sim[t][u])
-		prediction_second_denom += abs(sim[t][u])
+	if not math.isnan(rti):
+		prediction = r_mean[t]
 
-if prediction_second_denom != 0.0
-	prediction += ((prediction_second_num*1.0)/(prediction_second_denom*1.0))
-'''
+		prediction_second_num =0.0
+		prediction_second_denom =0.0
+
+
+		for u in U_nei:
+			rui = user_item.ix[u][i]
+			if not math.isnan(rui):
+				prediction_second_num += ((rui - r_mean[u]) * sim[t][u])
+				prediction_second_denom += abs(sim[t][u])
+			
+		if prediction_second_denom != 0.0:
+			prediction += ((prediction_second_num*1.0)/(prediction_second_denom*1.0))
+
+		#print r_mean[t]
+		#print prediction,"prediction"
+		#list_predictions.append((i,prediction))
+
+print list_predictions
